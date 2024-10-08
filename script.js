005faf3ad899a219a4f0ba2f55b018c24b49ed66
itsritaoliveira <160160643+itsritaoliveira@users.noreply.github.com>
2024-10-08 17:24:28 +0100

    Update

---
+++ script.js
@@ -0,0 +1,13 @@
+function updateTime() {
+  let losAngelesElement = document.querySelector("#los-angeles");
+  if (losAngelesElement) {
+    let losAngelesDateElement = losAngelesElement.querySelector(".date");
+    let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+    let losAngelesTime = moment().tz("America/Los_Angeles");
+
+    losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM	Do YYYY");
+    losAngelesTimeElement.innerHTML = losAngelesTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
+}
